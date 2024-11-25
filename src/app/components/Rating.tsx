346cc72cd62f3a4258e3170cc90a3d712a8d7732
Naif Otibi <nn.n@outlook.cl>
2024-11-26 02:16:15 +0300

    adding the redorange color

--- src/app/components/Rating.tsx
+++ src/app/components/Rating.tsx
@@ -6,7 +6,7 @@ export default function Rating() {
   return (
     <div className="flex flex-col gap-4 w-3/4 md:w-1/4  p-4 rounded-md bg-slate-700 shadow-sm absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <div className="w-fit p-4 rounded bg-slate-600">
-        <Star className="w-4 h-4 bg-" />
+        <Star className="w-4 h-4 text-[#FF3800]" />
       </div>
     </div>
   );
