39e60726a8bd23fb6c3484cebc529d8924bbfb29
Naif Otibi <nn.n@outlook.cl>
2024-11-26 02:21:57 +0300

    changing range of colors

--- src/app/components/Rating.tsx
+++ src/app/components/Rating.tsx
@@ -4,11 +4,11 @@ import { Star } from "lucide-react";
 
 export default function Rating() {
   return (
-    <div className="flex flex-col gap-4 w-3/4 md:w-1/4  p-4 rounded-md bg-slate-700 shadow-sm absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <div className="w-fit p-4 rounded bg-slate-600">
+    <div className="flex flex-col gap-4 w-3/4 md:w-1/4  p-4 rounded-md bg-slate-800 shadow-sm absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="w-fit p-4 rounded-full bg-slate-700">
         <Star className="w-4 h-4 text-[#FF3800]" />
       </div>
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-2">
         <h1 className="text-xl text-slate-100 font-medium">How Did we do?</h1>
         <p className="text-sm text-slate-00">Please let us know how we did your support request. All feedback is appreciated to help us imporve offering!</p>
       </div>
